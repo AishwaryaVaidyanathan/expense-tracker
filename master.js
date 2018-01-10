e45feb6d152a1d1c3707f23cb2bb516140aebf47
Sethu <xbsdeset@gmail.com>
2018-01-10 22:40:34 +0530

    first commit

---
+++ master.js
@@ -0,0 +1,40 @@
+var app = angular.module('expenseTracker', ['ui.router']);
+
+
+app.config(function ($stateProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(false);
+
+    var loginState = {
+        name: 'login',
+        url: '/',
+        templateUrl: 'templates/login.html'
+    }
+    $stateProvider.state(loginState);
+
+    var expensesState = {
+        name: 'expenses',
+        url: '/expenses',
+        templateUrl: 'templates/expenses.html'
+    }
+
+    $stateProvider.state(expensesState);
+
+
+});
+
+app.controller('expensesCtrl', function($scope) {
+    $scope.items = []
+    $scope.newItem = {date:new Date}
+    var balance=0;
+    $scope.addItem=function() {
+        balance += $scope.newItem.amount;
+        $scope.newItem.balance = balance;
+        $scope.items.push($scope.newItem)        
+        $scope.newItem = {date:new Date}
+    }
+
+ 
+});
+
+

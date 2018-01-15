0092b80a0c0815a57670098e46bb5720dafec9c9
gargisudame <34732852+gargisudame@users.noreply.github.com>
2018-01-15 20:28:23 +0530

    separated routes from master

---
+++ routes.js
@@ -0,0 +1,27 @@
+app.config(function ($stateProvider,$urlRouterProvider) {
+
+    $stateProvider
+        .state({
+            name: 'login',
+            url: '/',
+            templateUrl: 'views/login/login.html'
+        })
+        .state({
+            name: 'signup',
+            url: '/signup',
+            templateUrl: 'views/signup/signup.html'
+        })
+        .state({
+            name: 'expenses',
+            url: '/expenses',
+            templateUrl: 'views/expenses/expenses.html'
+        })
+        .state({
+            name: 'budget',
+            url: '/budget',
+            templateUrl: 'views/budget/budget.html'
+        })
+
+        $urlRouterProvider.otherwise('/');
+
+});

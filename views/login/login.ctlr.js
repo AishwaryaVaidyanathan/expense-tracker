c4beea4533efa34f8f93afb17966a6aef100a07e
Sethu <you@example.com>
2018-01-15 20:28:12 +0530

    created control for signup and login

--- views/login/login.ctlr.js
+++ views/login/login.ctlr.js
@@ -1,4 +1,16 @@
-app.controller('loginCtrl', function($scope) {
+app.controller('loginCtrl', function($scope, $http, $state) {
     // $scope.user = {}
+    $scope.login = function(){
+        
+        $http
+        .post('api/users/login.php',$scope.user)
+        .then(function(){
+            $state.go('budget')
+        })
+        .catch(function(err){
+            $scope.err_msg=err.data;
+        })
 
+
+    }
 });

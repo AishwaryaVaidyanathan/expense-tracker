479254092478c74558138d040c7b55ce2d8a0f35
Sethu <you@example.com>
2018-01-18 19:16:13 +0530

    final project ready

--- views/budget/budget.ctrl.js
+++ views/budget/budget.ctrl.js
@@ -1,5 +1,10 @@
 app.controller('budgetCtrl', function($scope,$http,$state) {
     // $scope.user = {}
+    $http
+    .post('api/budget/get.php')
+    .then(function(res){
+        $scope.budget=res.data.budget;  
+    })
     $scope.acceptBudget = function(){
         
         

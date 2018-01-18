b7a038c419be2b19586920a9f4cb3f9fb7c2c7d8
Sethu <you@example.com>
2018-01-18 15:09:35 +0530

    rectified codes

--- views/expenses/expense.ctrl.js
+++ views/expenses/expense.ctrl.js
@@ -43,10 +43,9 @@ app.controller('expensesCtrl', function($scope, $http) {
        
     }
 
-
     function totalSpent(){
         var spent=0
-        if($scope.items.forEach)(function(d){
+        $scope.items.forEach(function(d){
             spent+=(d.amount-0)
         })
         $scope.spent=spent;
